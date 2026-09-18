27ccd02c2d2dc25d2ddf3a3e272a6a42a6a8aca0
QwQe308 <3174905334@qq.com>
2026-09-18 13:29:44 +0800

    fix: AM production now shows properly when it's much lower than current AM

--- src/core/_MOD/space.js
+++ src/core/_MOD/space.js
@@ -82,7 +82,9 @@ export function produceAM(proc, diff) {
   Currency.antimatter.value = pendingTrueAM;
   player.records.totalAntimatter = player.records.totalAntimatter.max(player.antimatter);
 
-  player.amProc = player.antimatter.sub(recordAM).div(diff.div(1000)).max(0);
+  player.amProc = player.antimatter.sub(recordAM).div(diff.div(1000));
+
+  if(player.amProc.lte(player.antimatter.div(1e15))) player.amProc = proc.root(getSpaceNerf())
 
   // Some precision bugs happened after using BE and has to do so
   if (player.dimensions.antimatter[0].amount.eq(0)) Currency.antimatter.bumpTo(10);
